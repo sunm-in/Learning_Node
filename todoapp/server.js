abd093baef9cce3a3f7514bfc2cfefb0d6db225f
sunmin <sunm309@naver.com>
2021-12-05 21:07:05 +0900

    🔨 socket.io 추가

--- todoapp/server.js
+++ todoapp/server.js
@@ -1,43 +1,70 @@
 const express = require('express');
 const app = express();
+
+// socket.io
+const http = require('http').createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(http);
+
 app.use(express.urlencoded({ extended: true }));
+
 const MongoClient = require('mongodb').MongoClient;
 const methodOverride = require('method-override');
-const { ObjectId } = require('mongodb');
 app.use(methodOverride('_method'));
+
+const { ObjectId } = require('mongodb');
+
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 require('dotenv').config();
+
+app.use(express.json());
+const cors = require('cors');
+app.use(cors());
 
 // app.listen(8080, () => {
 //   console.log('listening on 8080');
 // });
 
 let db;
-MongoClient.connect(
-  process.env.DB_URL,
-  { useUnifiedTopology: true },
-  (error, client) => {
-    // 연결되면 할일
-    if (error) return console.log(error);
-    db = client.db('todoapp');
+MongoClient.connect(process.env.DB_URL, { useUnifiedTopology: true }, (error, client) => {
+  if (error) return console.log(error);
+  db = client.db('todoapp');
+  app.db = db;
 
-    // db.collection('post').insertOne(
-    //   { 이름: 'John', _id: 100 },
-    //   (error, result) => {
-    //     console.log('저장완료');
-    //   }
-    // );
+  http.listen(process.env.PORT, () => {
+    console.log('listening on server');
+  });
+});
 
-    app.listen(process.env.PORT, function () {
-      console.log('listening on server');
-    });
-  }
-);
+// socket
+app.get('/socket', (req, res) => {
+  res.render('socket.ejs');
+});
 
-// app.get('/임시', function (req, res) {
-//   res.sendFile(__dirname + '/')
-// });
+io.on('connection', (socket) => {
+  console.log('접속 완료');
+
+  // room1에 있는 유저에게만 전송
+  socket.on('room1-send', (data) => {
+    io.to('room1').emit('broadcast', data);
+  });
+
+  // 채팅방 만들기
+  socket.on('joinroom', (data) => {
+    socket.join('room1');
+  });
+
+  socket.on('user-send', (data) => {
+    // client -> server
+    // user-send 이름으로 메세지 보내면 내부 코드 실행
+    console.log(data);
+
+    // server -> client
+    io.emit('broadcast', data); // 단체 채팅방 개념
+    // io.to(socket.id).emit('broadcast', data); // 개인 채팅방
+  });
+});
 
 // 메인페이지
 app.get('/', (req, res) => {
@@ -60,31 +87,24 @@ app.get('/list', (req, res) => {
   db.collection('post')
     .find()
     .toArray((err, result) => {
-      // console.log(result);
       res.render('list.ejs', { posts: result });
     });
 });
 
 // 상세페이지
 app.get('/detail/:id', (req, res) => {
-  db.collection('post').findOne(
-    { _id: parseInt(req.params.id) },
-    (err, result) => {
-      console.log(result);
-      res.render('detail.ejs', { data: result });
-    }
-  );
+  db.collection('post').findOne({ _id: parseInt(req.params.id) }, (err, result) => {
+    console.log(result);
+    res.render('detail.ejs', { data: result });
+  });
 });
 
 // 게시물 수정
 app.get('/edit/:id', (req, res) => {
-  db.collection('post').findOne(
-    { _id: parseInt(req.params.id) },
-    (err, result) => {
-      console.log(result);
-      res.render('edit.ejs', { post: result });
-    }
-  );
+  db.collection('post').findOne({ _id: parseInt(req.params.id) }, (err, result) => {
+    console.log(result);
+    res.render('edit.ejs', { post: result });
+  });
 });
 
 // form에 담긴 제목, 날짜 데이터를 가지고 db.collection에 업데이트하기
@@ -126,9 +146,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
 
-app.use(
-  session({ secret: 'secretCode', resave: true, saveUninitialized: false })
-);
+app.use(session({ secret: 'secretCode', resave: true, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -178,12 +196,10 @@ passport.use(
       passReqToCallback: false,
     },
     (userId, userPw, done) => {
-      // console.log(userId, userPw);
       db.collection('login').findOne({ id: userId }, (err, result) => {
         if (err) return done(err);
 
-        if (!result)
-          return done(null, false, { message: '존재하지 않는 아이디 입니다.' });
+        if (!result) return done(null, false, { message: '존재하지 않는 아이디 입니다.' });
 
         if (userPw == result.pw) {
           return done(null, result);
@@ -210,17 +226,13 @@ passport.deserializeUser((id, done) => {
 
 // 저장 전에 id가 이미 있는지 체크하기, id에 알파벳 숫자만 잘 들어있나 체크, 비밀번호 저장 전에 암호화를 했는지 체크
 app.post('/register', (req, res) => {
-  db.collection('login').insertOne(
-    { id: req.body.id, pw: req.body.pw },
-    (err, result) => {
-      res.redirect('/');
-    }
-  );
+  db.collection('login').insertOne({ id: req.body.id, pw: req.body.pw }, (err, result) => {
+    res.redirect('/');
+  });
 });
 
 // post 작성
 app.post('/add', (req, res) => {
-  // res.send('전송완료');
   db.collection('counter').findOne({ name: 'postLength' }, (err, result) => {
     let postNumber = result.totalPost;
     let postInfo = {
@@ -289,7 +301,7 @@ app.get('/image/:imageName', (req, res) => {
   res.sendFile(__dirname + '/public/image/' + req.params.imageName);
 });
 
-// 채팅
+// 채팅 (SSE)
 app.post('/chatroom', isLogin, (req, res) => {
   let chatInfo = {
     title: '채팅방이름',
@@ -336,4 +348,33 @@ app.post('/message', isLogin, (req, res) => {
     .catch((err) => {
       console.error(err);
     });
+});
+
+// SSE(Server Sent Events)
+app.get('/message/:id', isLogin, (req, res) => {
+  res.writeHead(200, {
+    'Connection': 'keep-alive',
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+  });
+
+  db.collection('message')
+    .find({ parent: req.params.id })
+    .toArray()
+    .then((result) => {
+      res.write('event: test\n');
+      res.write('data: ' + JSON.stringify(result) + '\n\n');
+    });
+
+  const pipeline = [
+    // 컬렌션 안의 원하는 document만 감시하고 싶은걸 작성 (fullDocument 붙여주기)
+    { $match: { 'fullDocument.parent': req.params.id } },
+  ];
+  const collection = db.collection('message');
+  const changeStream = collection.watch(pipeline); // .watch() 실시간 감지
+  changeStream.on('change', (result) => {
+    // 해당 컬렉션에 변동 생기면 여기에 있는 코드 실행됨
+    res.write('event: test\n');
+    res.write('data: ' + JSON.stringify([result.fullDocument]) + '\n\n');
+  });
 });
