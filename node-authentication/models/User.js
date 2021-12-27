925846ea4f9e98d48e408c1b4a9fe248ea1f787a
sunmin <sunm309@naver.com>
2021-12-27 22:13:27 +0900

    🔨 user schema 작성

---
+++ node-authentication/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true, // 중간 공백을 지워줌
+    unipue: 1, // 중복 이메일 체크
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User };
