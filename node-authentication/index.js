a190eadad0d86d841ba221c8f447603fd8770fbb
sunmin <sunm309@naver.com>
2021-12-27 22:14:00 +0900

    🔨 MongoDB 연결

--- node-authentication/index.js
+++ node-authentication/index.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
+const { PORT, DB_URL } = process.env;
+
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = PORT || 4000;
 
+app.use(express.json());
 const cors = require('cors');
 app.use(cors());
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.error(err));
+
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
