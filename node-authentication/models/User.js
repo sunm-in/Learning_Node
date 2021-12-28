09c8c0a05cf2f85f81c059f2341d493901b912a9
sunmin <sunm309@naver.com>
2021-12-28 18:11:59 +0900

    🔨 비밀번호 암호화, 토큰 생성

--- node-authentication/models/User.js
+++ node-authentication/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -9,6 +12,10 @@ const userSchema = mongoose.Schema({
     type: String,
     trim: true, // 중간 공백을 지워줌
     unipue: 1, // 중복 이메일 체크
+  },
+  password: {
+    type: String,
+    minlength: 8,
   },
   lastname: {
     type: String,
@@ -26,6 +33,42 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    // password가 변경될 때만 Hashing 실행
+    // genSalt: salt 생성
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hashedPassword) {
+        if (err) return next(err);
+        user.password = hashedPassword;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  const user = this;
+  const token = jwt.sign(user._id.toHexString(), 'secretToken');
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 const User = mongoose.model('User', userSchema);
 
