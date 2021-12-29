84f11434c65463da95f92c0508fea7c775a08573
sunmin <sunm309@naver.com>
2021-12-29 22:00:17 +0900

    🔨 auth 기능

--- node-authentication/middleware/auth.js
+++ node-authentication/middleware/auth.js
@@ -1,5 +1,15 @@
+const { User } = require('../models/User');
+
 const auth = (req, res, next) => {
-  // 인증 처리
+  let token = req.cookies.x_auth;
+  User.findByToken(token, (err, user) => {
+    if (err) throw err;
+    if (!user) return res.json({ isAuth: false, error: true });
+
+    req.token = token;
+    req.user = user;
+    next();
+  });
 };
 
 // export default auth;
--- node-authentication/models/User.js
+++ node-authentication/models/User.js
@@ -39,10 +39,10 @@ userSchema.pre('save', function (next) {
   if (user.isModified('password')) {
     // password가 변경될 때만 Hashing 실행
     // genSalt: salt 생성
-    bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
       if (err) return next(err);
 
-      bcrypt.hash(user.password, salt, function (err, hashedPassword) {
+      bcrypt.hash(user.password, salt, (err, hashedPassword) => {
         if (err) return next(err);
         user.password = hashedPassword;
         next();
@@ -54,7 +54,7 @@ userSchema.pre('save', function (next) {
 });
 
 userSchema.methods.comparePassword = function (plainPassword, cb) {
-  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
     if (err) return cb(err);
     cb(null, isMatch);
   });
@@ -64,9 +64,21 @@ userSchema.methods.generateToken = function (cb) {
   const user = this;
   const token = jwt.sign(user._id.toHexString(), 'secretToken');
   user.token = token;
-  user.save(function (err, user) {
+  user.save((err, user) => {
     if (err) return cb(err);
     cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  const user = this;
+
+  // 토큰 decode
+  jwt.verify(token, 'secretToken', function (err, decoded) {
+    user.findOne({ _id: decoded, token: token }, (err, user) => {
+      if (err) return cb(err);
+      cb(null, user);
+    });
   });
 };
 
