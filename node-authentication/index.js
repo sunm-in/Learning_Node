a0470225a0f9ddf703b88cfde871be953f4f3bcf
sunmin <sunm309@naver.com>
2021-12-29 21:59:22 +0900

    🔨 router추가 - auth, logout

--- node-authentication/index.js
+++ node-authentication/index.js
@@ -21,6 +21,10 @@ mongoose
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.error(err));
 
+http.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
@@ -60,6 +64,25 @@ app.post('/api/users/login', (req, res) => {
   });
 });
 
-http.listen(port, () => {
-  console.log(`listening on ${port}`);
+app.get('/api/users/auth', auth, (req, res) => {
+  // authentication 이 true일 때 클라이언트에 보내줄 정보
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true, // 0이면 일반유저, 0이 아니면 관리자
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
 });
