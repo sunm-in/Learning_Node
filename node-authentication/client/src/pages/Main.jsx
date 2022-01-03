65522aa50c3d2fda1849edc920d38cfd67988b86
sunmin <sunm309@naver.com>
2022-01-03 17:27:37 +0900

    🔨 로그아웃 기능 구현

--- node-authentication/client/src/pages/Main.jsx
+++ node-authentication/client/src/pages/Main.jsx
@@ -1,14 +1,35 @@
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 const Main = () => {
+  const naivgate = useNavigate();
   useEffect(() => {
     axios.get('/api/hello').then((res) => console.log(res.data));
   }, []);
 
+  const $logOut = () => {
+    axios.get('/api/users/logout').then((res) => {
+      if (res.data.success) {
+        naivgate('/login');
+      } else {
+        alert('페이지에 문제가 발생했습니다.');
+      }
+    });
+  };
+
   return (
-    <>
-      <h1>Main!!!!</h1>
-    </>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <h1>Main</h1>
+      <button onClick={$logOut}>로그아웃</button>
+    </div>
   );
 };
 
