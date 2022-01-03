66321fd937af4f640134bcbf5b0b2c548e9b9108
sunmin <sunm309@naver.com>
2022-01-03 17:28:46 +0900

    🔨 Auth 적용

--- node-authentication/client/src/route/Router.jsx
+++ node-authentication/client/src/route/Router.jsx
@@ -7,15 +7,18 @@ import { NavBar, Footer } from '../components';
 // pages
 import { Main, Login, Register, NotFound } from '../pages';
 
+// hoc
+import Auth from '../shared/hoc/auth';
+
 const Router = () => {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
         <Route path='/*' element={<NotFound />} />
-        <Route path='/' element={<Main />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+        <Route path='/' element={Auth(Main, null)} />
+        <Route path='/login' element={Auth(Login, false)} />
+        <Route path='/register' element={Auth(Register, false)} />
       </Routes>
       <Footer />
     </BrowserRouter>
